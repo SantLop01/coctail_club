250f67e4c55d0e837ed79dee03099f85845abf6f
Santiago López <gabsant2001@gmail.com>
2024-01-11 17:17:31 -0300

    [FIX] Change to secure https for deploy

--- js/api.js
+++ js/api.js
@@ -2,7 +2,7 @@
 
 export const getRandomDrink = async () => {
     try {
-        const response = await fetch('http://www.thecocktaildb.com/api/json/v1/1/random.php');
+        const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
         const data = await response.json();
         // const drink = data.drinks[0];
         return data;
@@ -13,7 +13,7 @@ export const getRandomDrink = async () => {
 
 export const getAllCategories = async (cat) => {
     try {
-        const response = await fetch('http://www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
+        const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
         const data = await response.json();
         return data;
     } catch (error) {
@@ -23,7 +23,7 @@ export const getAllCategories = async (cat) => {
 
 export const getAllIngredients = async () => {
     try {
-        const response = await fetch(`http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list`);
+        const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list`);
         const data = await response.json();
         return data;
     } catch (error) {
@@ -33,7 +33,7 @@ export const getAllIngredients = async () => {
 
 export const getAllGlasses = async () => {
     try {
-        const response = await fetch('http://www.thecocktaildb.com/api/json/v1/1/list.php?g=list');
+        const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?g=list');
         const data = response.json();
         return data;
     } catch (error) {
@@ -45,7 +45,7 @@ export const getAllGlasses = async () => {
 
 export const filterByCategory = async (cat) => {
     try {
-        const response = await fetch(`http://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${cat}`);
+        const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${cat}`);
         const data = await response.json();
         return data;
     } catch (error) {
@@ -55,7 +55,7 @@ export const filterByCategory = async (cat) => {
 
 export const filterByIngredient = async (ing) => {
     try {
-        const response = await fetch(`http://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ing}`);
+        const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ing}`);
         const data = await response.json();
         return data;
     } catch (error) {
@@ -65,7 +65,7 @@ export const filterByIngredient = async (ing) => {
 
 export const filterByIdDrink = async (id) => {
     try {
-        const response = await fetch(`http://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
+        const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
         const data = await response.json();
         const drink = data.drinks[0];
         return drink;
@@ -76,7 +76,7 @@ export const filterByIdDrink = async (id) => {
 
 export const filterByGlass = async (glass) => {
     try {
-        const response = await fetch(`http://www.thecocktaildb.com/api/json/v1/1/filter.php?g=${glass}`);
+        const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?g=${glass}`);
         const data = await response.json();
         const drink = data.drinks;
         return drink;
